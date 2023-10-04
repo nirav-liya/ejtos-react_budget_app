6d34fd86d07ba09061e922123be292efc2a18917
nirav-liya <nirav.liya@gmail.com>
2023-10-04 02:54:01 -0400

    prg

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,9 +2,24 @@ import React, { useContext,useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = (props) => {
-    const { dispatch, totalBudget, Currency } = useContext(AppContext);
+    const { dispatch, totalBudget, Currency,budgets } = useContext(AppContext);
     const [budget, setBudget] = useState('');
     const changeTotalBudget = (val)=>{
+        const totalBudgetSpend = budgets.reduce((total, item) => {
+            return (total += item.quantity);
+        }, 0);
+        
+        if (parseInt(val)>100)
+        {
+            alert('Budget Value can not exceed 20,000');
+            return;
+        }
+
+        if (parseInt(val)<totalBudgetSpend)
+        {
+            alert('You can not reduce the budget value lower then spending');
+            return;
+        }
         dispatch({
             type: 'CHG_TOTALBUDGET',
             payload: val,
@@ -18,7 +33,8 @@ const Budget = (props) => {
                         required='required'
                         type='number'
                         id='budget'
-                        
+                        step='10'
+                        value={totalBudget}
                         style={{size: 6}}
                         onChange={(event) => changeTotalBudget(event.target.value)}>
                         </input>
